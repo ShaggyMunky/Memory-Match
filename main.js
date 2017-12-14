13893e8fa69513ac5088816268db11872d58b6ed
Aaron Guillen <aaronguillen21@gmail.com>
2017-12-13 17:58:48 -0800

    Memory Match v0.1 - Aaron Guillen

---
+++ main.js
@@ -0,0 +1,18 @@
+// $(document).ready(main);
+//
+// function main(){
+//     adjustParentToChild();
+//
+// }
+//
+// function adjustParentToChild(){
+//     var parentHeight = "0";
+//
+//     $(".card *").each(function(){
+//         if (this.height() > parentHeight){
+//             parentHeight = this.height();
+//         }
+//     });
+//
+//     return $(".card").height(parentHeight);
+// }
